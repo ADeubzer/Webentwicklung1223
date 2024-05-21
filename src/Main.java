2552881ba3ee4a55d91a440ce8d4ab007e718fdb
AndiDe <mail@andreasdeubzer.de>
2024-05-21 15:34:41 +0200

    User wird mit Eingabefenstern initiiert.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import static javax.swing.JOptionPane.showInputDialog;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        User username = new User(showInputDialog("Bitte geben Sie Ihren Username ein: "),
+                showInputDialog("Bitte geben Sie Ihre Emailadresse ein:"),
+                showInputDialog("Bitte geben Sie Ihr Passwort ein:"));
+
+        System.out.println("Benutzename: " + username.username +
+                "\nEmail: " +username.email +
+                "\nPassswort: " + username.password);
     }
 }
